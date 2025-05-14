f726ae9e59f20dc4246cd4aec437513429339e15
jsenaribeiro <jsenaribeiro@gmail.com>
2025-05-13 21:03:17 -0300

    'doc....'

--- readme/index.js
+++ readme/index.js
@@ -1,10 +1,7 @@
-function startup() {   
-   const iframe = document.querySelector('iframe')
-   const content = iframe.contentWindow.document.body
+console.log('index.js')
 
-   adjustZoom(content)
+function startup() {   
    createLogo(document)
-   // createLink(iframe, content)
 }
 
 function goto(href) {
@@ -27,20 +24,4 @@ function createLink(iframe, content) {
    const click = a => { apply(a); refix(); }
 
    links.forEach(a => a.onclick = () => click(a))
-}
-
-function createLogo(content) {
-   const logo = content.querySelector("logo")
-   const divs = [1,2,3].map(() => document.createElement('div'))
-   
-   divs.concat([document.createElement('span')])   
-       .forEach(x => logo.appendChild(x))
-
-   setInterval(function() {
-      const from0To3 = Math.floor(Math.random() * 3)
-      const during = 150 + (100 * (from0To3 * 9))
-
-      document.querySelectorAll("logo div")
-         .forEach(div => div.style.animationDuration = `${during}ms`)
-   }, 3000)
 }
