a980434e4920b6081d91ad0c93054a8b6bb4c225
jsenaribeiro <jsenaribeiro@gmail.com>
2025-05-17 16:04:07 -0300

    'doc....'

---
+++ readme/folder.md
@@ -0,0 +1,16 @@
+<style>@import url(./folder.css);</style> 
+<style>@import url(./index.css);</style> 
+
+<section class='infos'>
+
+|   |                                       |
+| ----------- | --------------------------------------------------- |
+| /apis       | RESTful function requests                           |
+| /assets     | Public static content: images, sounds, fonts, etc   |
+| /routes     | Component pages for directory routing               |
+| /builds     | Ready-to-deploy post-build content                  |
+| /controls   | Standard shareable control components               |
+| /directives | Injectable props (Angular-like attribute directive) |
+| /decorators | Injectable function decorators (transpiler)         |
+
+</section>
--- readme/index.js
+++ readme/index.js
@@ -1,27 +1,14 @@
-console.log('index.js')
+const goto = window.parent.goto
 
-function startup() {   
-   createLogo(document)
-}
+window.addEventListener('DOMContentLoaded', () => resize(false));
 
-function goto(href) {
-   console.log({ href })
-   document.querySelector('main[iframe]').hidden = false
-   document.querySelector('iframe').src = href
-   document.querySelector('main[index]').hidden = true
-}
+function resize(remake) {
+   const repeat = () => resize(true)
+   const iframe = document.querySelector('#folder')
+   const target = iframe?.contentWindow?.document?.body
+   const height = target?.scrollHeight + 'px'
 
-function adjustZoom(content) {
-   content.style.zoom = 1.17
-   content.style.padding = '50px !important'   
-}
-
-function createLink(iframe, content) {
-   const links = content.querySelectorAll('[overview] a')
-   const found = x => x.href.split('#').at(-1) ?? ''
-   const apply = a => iframe.src = `./preview.html#${found(a)}`
-   const refix = () => setTimeout(() => adjustZoom(content), 999)
-   const click = a => { apply(a); refix(); }
-
-   links.forEach(a => a.onclick = () => click(a))
+   if (!height || !iframe) return
+   else iframe.style.height = height
+   if (!remake) setTimeout(repeat, 222)
 }
